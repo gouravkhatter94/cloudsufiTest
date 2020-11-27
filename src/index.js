e37c285114aaae22937581cc15c589f168f51819
Gourav Khatter <gourav.khatter@ticketmaster.com>
2020-11-27 15:57:14 +0530

    change

--- src/index.js
+++ src/index.js
@@ -33,19 +33,60 @@ module.exports.handler = async (event, context) => {
         const dbData = await getData();
         let {zipcode, cityName, latitude, longitude, type, state, county, country, timezone, populationGreaterThan} = event.queryStringParameters;
 
-        if (zipcode) {
-            objResponse.data = dbData.filter(objZipCode => objZipCode.zip.indexOf(zipcode) > -1);
-        } else if (cityName) {
-            cityName = cityName && cityName.toLowerCase().trim();
-            objResponse.data = dbData.filter(objZipCode => objZipCode.primary_city.toLowerCase().indexOf(cityName) > -1);
-        } else if (latitude || longitude) {
+        // filter by optional field type, state, county, country, timezone (atleast one require)
+        zipcode = zipcode && zipcode.toLowerCase().trim();
+        cityName = cityName && cityName.toLowerCase().trim();
+        type = type && type.toLowerCase().trim();
+        state = state && state.toLowerCase().trim();
+        county = county && county.toLowerCase().trim();
+        country = country && country.toLowerCase().trim();
+        timezone = timezone && timezone.toLowerCase().trim();
 
-            // if one out of latitude or longitude is not there we assume that as 0
-            latitude = latitude || 0;
-            longitude = longitude || 0;
+        objResponse.data = dbData.filter(objZipCode => {
+            let isPassed = true;
+
+            if (type) {
+                isPassed = objZipCode.type.toLowerCase().indexOf(type) > -1;
+            }
+
+            if (isPassed && state) {
+                isPassed = objZipCode.state.toLowerCase().indexOf(state) > -1;
+            }
+
+            if (isPassed && county) {
+                isPassed = objZipCode.county.toLowerCase().indexOf(county) > -1;
+            }
+
+            if (isPassed && country) {
+                isPassed = objZipCode.country.toLowerCase().indexOf(country) > -1;
+            }
+
+            if (isPassed && timezone) {
+                isPassed = objZipCode.timezone.toLowerCase().indexOf(timezone) > -1;
+            }
+
+            if (isPassed && populationGreaterThan) {
+                isPassed = Number(objZipCode.estimated_population) > populationGreaterThan;
+            }
+
+            if (isPassed && zipcode) {
+                isPassed = objZipCode.zip.indexOf(zipcode) > -1;
+            }
+
+            if (isPassed && cityName) {
+                isPassed = objZipCode.primary_city.toLowerCase().indexOf(cityName) > -1;
+            }
+
+            return isPassed;
+        });
+
+        if (latitude || longitude) {
+                // if one out of latitude or longitude is not there we assume that as 0
+            latitude = latitude.trim() || 0;
+            longitude = longitude.trim() || 0;
 
             // Get all cities distance from given lat and long
-            const arrDistantCities = dbData.map(objZipCode => {
+            const arrDistantCities = objResponse.data.map(objZipCode => {
                 objZipCode.distance = distance(objZipCode.latitude, objZipCode.longitude, latitude, longitude, "K");
                 return objZipCode;
             });
@@ -54,46 +95,7 @@ module.exports.handler = async (event, context) => {
             const arrSortedDistantCities = arrDistantCities.sort((a, b) => a.distance - b.distance);
 
             objResponse.data = arrSortedDistantCities[0];
-        } else if (type || state || county || country || timezone || populationGreaterThan) {
-            // filter by optional field type, state, county, country, timezone (atleast one require)
-
-            type = type && type.toLowerCase().trim();
-            state = state && state.toLowerCase().trim();
-            county = county && county.toLowerCase().trim();
-            country = country && country.toLowerCase().trim();
-            timezone = timezone && timezone.toLowerCase().trim();
-
-            objResponse.data = dbData.filter(objZipCode => {
-                let isPassed = true;
-
-                if (type) {
-                    isPassed = objZipCode.type.toLowerCase().indexOf(type) > -1;
-                }
-
-                if (isPassed && state) {
-                    isPassed = objZipCode.state.toLowerCase().indexOf(state) > -1;
-                }
-
-                if (isPassed && county) {
-                    isPassed = objZipCode.county.toLowerCase().indexOf(county) > -1;
-                }
-
-                if (isPassed && country) {
-                    isPassed = objZipCode.country.toLowerCase().indexOf(country) > -1;
-                }
-
-                if (isPassed && timezone) {
-                    isPassed = objZipCode.timezone.toLowerCase().indexOf(timezone) > -1;
-                }
-
-                if (isPassed && populationGreaterThan) {
-                    isPassed = Number(objZipCode.estimated_population) > populationGreaterThan;
-                }
-
-                return isPassed;
-            });
         }
-
     } catch (error) {
         console.error("Error", error);
         objResponse.errorCode = 400;
